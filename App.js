a14c7ad6765f77f97c16805be656bf5bf176157d
Eric Montzka <emontzka@sbcglobal.net>
2019-03-10 19:15:40 -0500

    feat: add homepage Deck component

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+class Deck extends Component {
+  render() {
+    return (
+      <View style={styles.deck}>
+      <Text style={styles.deckTitle} >{this.props.title}</Text>
+      <Text style={{textAlign: 'center'}}>{this.props.cards} cards</Text>
+      </View>
+
+      
+    );
+  }
+}
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Deck  title='Deck One' cards='3' />
+        <Deck  title='Deck Two' cards='4' />
       </View>
     );
   }
@@ -18,4 +34,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  deck: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    marginTop: 20,
+    marginBottom: 20
+  },
+  deckTitle: {
+    fontSize: 24,
+    color: 'blue'
+  }
 });
+
